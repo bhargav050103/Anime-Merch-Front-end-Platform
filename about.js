3eaae327068b60cc4ba0b1502845948e1cb8ed41
J Bhargav Ram <124713968+bhargavram0501@users.noreply.github.com>
2025-03-08 23:34:38 +0530

    Anime Merch Code and images

---
+++ about.js
@@ -0,0 +1,6 @@
+function fn()
+{
+    // const toggleButton = document.getElementById('toggle-button')
+    const navbarLinks = document.getElementById('navbar-links')
+    navbarLinks.classList.toggle('active')
+}
